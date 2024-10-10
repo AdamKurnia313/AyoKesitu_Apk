9d7c1fe47357667f43f13d39bbfbe0ff2a957d9f
DEFAN DEO VANNES SEPTA LAILAL MI'RAJ <133806931+defandeovan@users.noreply.github.com>
2024-10-10 21:38:58 +0700

    Update Home_Screen.dart

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -124,6 +124,8 @@ class HomeScreenBody extends StatelessWidget {
           _buildPopularActivities(),
         ],
       ),
+              
+
     );
   }
 
@@ -278,64 +280,101 @@ class HomeScreenBody extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildRecommendationCard(Map<String, String> recommendation) {
-    return Container(
-      width: 210,
-      height: 249,
-      margin: EdgeInsets.only(right: 16),
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Color(0xfeD9D9D9),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset(
-                recommendation["image"]!,
-                width: 186,
-                height: 132,
-                fit: BoxFit.cover,
+Widget _buildRecommendationCard(Map<String, String> recommendation) {
+  return Container(
+    width: 210,
+    height: 249,
+    margin: EdgeInsets.only(right: 16),
+    padding: EdgeInsets.all(12),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(12),
+      color: Color(0xfeD9D9D9),
+    ),
+    child: Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  recommendation["image"]!,
+                  width: 186,
+                  height: 132,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 8),
-          Text(
-            recommendation["title"]!,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 4),
-          Row(
+            SizedBox(height: 8),
+            Text(
+              recommendation["title"]!,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                Image.asset('assets/img/Map Pin.png'),
+                SizedBox(width: 4),
+                Text(
+                  recommendation["location"]!,
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontFamily: 'Conamore',
+                      fontWeight: FontWeight.normal),
+                ),
+              ],
+            ),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                SvgPicture.asset('assets/img/star.svg'),
+                Text(
+                  recommendation['rating']!,
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
+        // Lingkaran dengan ikon di atasnya
+        Positioned(
+          top: 10, // Jarak dari atas
+          right: 10, // Jarak dari kanan
+          child: Stack(
+            alignment: Alignment.center, // Agar ikon berada di tengah lingkaran
             children: [
-              Image.asset('assets/img/Map Pin.png'),
-              SizedBox(width: 4),
-              Text(
-                recommendation["location"]!,
-                style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontFamily: 'Conamore',
-                    fontWeight: FontWeight.normal),
+              // Lingkaran
+              Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 0,
+                      blurRadius: 4,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
               ),
+              // Ikon di atas lingkaran
+               SvgPicture.asset(
+                      'assets/img/favorite.svg',
+                      width: 24,
+                      height: 24,
+                    ),
             ],
           ),
-          SizedBox(height: 4),
-          Row(
-            children: [
-              SvgPicture.asset('assets/img/star.svg'),
-              Text(
-                recommendation['rating']!,
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
+}
+
 
   Widget _buildPopularActivities() {
     final popularActivities = [
